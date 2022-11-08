44fe6377443d819d33265faacf52f08611d8de6d
rob-r-doyle <rob.r.doyle@gmail.com>
2022-11-08 11:36:37 -0500

    added rudimentary mochawesome reporting

--- wdio.conf.js
+++ wdio.conf.js
@@ -153,7 +153,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['mochawesome', {
+            outputDir: './Results'
+        }],
+    ],
 
 
     
